7136d5a1044db39699b0f0ab4ff38e3a66f5eaca
Sean Lai <seanlai221@gmail.com>
2025-11-13 08:53:36 -0500

    Trying to fix weird blackout on mobile

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -79,7 +79,7 @@ const Hero = () => {
           muted
           preload="auto"
         >
-          <source src="/hero_2025_fall.webm" type="video/webm" />
+          <source src="/hero.webm" type="video/webm" />
         </video>
         
         {/* Dark overlay to make text more readable */}
